ccba5513abdac59b15e0f40f6a4e2bf63c38ac17
Andrii <vlasandrey878@gmail.com>
2019-05-28 17:09:59 +0300

    28.05.2019 (dump db add)

--- Верстка/DUMPDB/kokomoodb.sql
+++ Верстка/DUMPDB/kokomoodb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Май 21 2019 г., 16:52
+-- Время создания: Май 28 2019 г., 17:09
 -- Версия сервера: 5.6.41
--- Версия PHP: 7.1.22
+-- Версия PHP: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -50,15 +50,22 @@ CREATE TABLE `artists` (
   `artist_id` int(10) UNSIGNED NOT NULL,
   `full_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `specialization` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `date_birthday` date NOT NULL
+  `date_birthday` date NOT NULL,
+  `image` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `slug` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Дамп данных таблицы `artists`
 --
 
-INSERT INTO `artists` (`artist_id`, `full_name`, `specialization`, `date_birthday`) VALUES
-(1, 'Alex Greenberger', 'German mannerist painter', '2019-05-14');
+INSERT INTO `artists` (`artist_id`, `full_name`, `specialization`, `date_birthday`, `image`, `slug`) VALUES
+(1, 'Alex Greenberger', 'German mannerist painter', '2019-05-14', '1558944906.jpeg', 'alexgreenberger'),
+(2, 'Jon Woe', 'Designer Web', '2019-05-06', '1558943337.jpeg', 'jonwhoe'),
+(3, 'Jon Doe', 'Designer Web', '2019-05-08', '1559049619.jpeg', 'jondoeslug'),
+(4, 'Mikel Paul', 'Java Script', '2019-05-02', '1559049641.jpeg', 'mikelpaul'),
+(5, 'Mike Tison', 'Danish artist and antiquarian', '2019-05-06', '1559049683.jpeg', 'miketison'),
+(6, 'Mikel Paul', 'Designer Web', '2019-05-04', '1559049704.jpeg', 'mikepaulsecond');
 
 -- --------------------------------------------------------
 
@@ -134,7 +141,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (23, '2019_05_08_102301_create_news_artists_table', 1),
 (24, '2019_05_08_132048_create_news_contents_table', 1),
 (25, '2019_05_15_070217_create_agencies_table', 1),
-(26, '2019_05_16_081507_create_main_contents_table', 1);
+(26, '2019_05_16_081507_create_main_contents_table', 1),
+(27, '2019_05_27_074319_add_image_to_artists_table', 2);
 
 -- --------------------------------------------------------
 
@@ -156,7 +164,9 @@ CREATE TABLE `news_artists` (
 --
 
 INSERT INTO `news_artists` (`news_id`, `artist_id`, `slug`, `category_english`, `category_china`, `date_news`) VALUES
-(1, 1, 'Event', 'Event Horizon: Art Events Around Bejing', '事闘人守述訪身界写局頑閉入別選数競願話', '2019-05-14');
+(1, 1, 'Event', 'Event Horizon: Art Events Around Bejing', '事闘人守述訪身界写局頑閉入別選数競願話', '2019-05-14'),
+(2, 2, 'loremipsumnews', 'Lorem Ipsum is simply dummy text of the printing and typesetting', '也称乱数假文或者哑元文本', '2019-05-28'),
+(3, 1, 'contrayloremslug', 'Contrary to popular belief, Lorem Ipsum', '是印刷及排版领域所常', '2019-05-28');
 
 -- --------------------------------------------------------
 
@@ -183,7 +193,11 @@ INSERT INTO `news_contents` (`content_id`, `news_id`, `photo`, `photo_title`, `t
 (1, 1, '1558444103.jpeg', NULL, '回第作夕整力開中関要志将英。倒模後始自治 回第作夕整力開中関要志将英。倒模後始自治', 'Earlier this month, Steven Tananbaum sued the gallery and Koons’s studio for the “non-delivery” of three sculptures.', NULL, NULL),
 (2, 1, '1558444103.jpeg', NULL, 'Wikipedia: A mural is any piece of artwork painted or applied directly on a wall, ceiling or other permanent surface. A distinguishing characteristic of mural painting is that the architectural elements of the given space are harmoniously incorporated into the picture.', '壁画是最古老的绘画形式;自旧石器时代以来就已被人类知晓。人们总是画房子的墙壁装饰或记录历史。这在宗教中特别流行，壁画讲述了耶稣的生平，圣经故事和人们生活在圣殿中的故事（它也构成了几乎所有宗教的一部分)', NULL, NULL),
 (3, 1, '1558444103.jpeg', NULL, NULL, NULL, NULL, NULL),
-(4, 1, '1558444103.jpeg', 'Diego Rivera Murals in  Mexico 1922-1929', NULL, NULL, NULL, NULL);
+(4, 1, '1558444103.jpeg', 'Diego Rivera Murals in  Mexico 1922-1929', NULL, NULL, NULL, NULL),
+(5, 2, '1559048293.jpeg', 'Title for the photo', '是印刷及排版领域所常用的虚拟文字。由于曾经', 'It is a long established fact that a reader will be distracted by the readable content', NULL, NULL),
+(6, 2, '1559048293.jpeg', 'Title for the photo', NULL, NULL, NULL, NULL),
+(7, 3, '1559048360.jpeg', NULL, NULL, NULL, NULL, NULL),
+(8, 3, '1559048360.jpeg', 'Title to the photo', '是印刷及排版领域所常', 'Contrary to popular belief, Lorem Ipsum', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -300,7 +314,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `type`, `bio`, `photo`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Vlasiuk Andrii', 'kaktys88@yandex.ru', NULL, '$2y$10$P7MCPqrCVfBjRVjxozVzqex6vd9G.npTvZmcOWEVBfjwu1Mo4TTXm', 'admin', NULL, 'camera.png', NULL, '2019-05-21 10:04:43', '2019-05-21 10:04:43');
+(1, 'Vlasiuk Andrii', 'kaktys88@yandex.ru', NULL, '$2y$10$P7MCPqrCVfBjRVjxozVzqex6vd9G.npTvZmcOWEVBfjwu1Mo4TTXm', 'user', NULL, 'camera.png', NULL, '2019-05-21 10:04:43', '2019-05-22 05:27:21'),
+(2, 'Admin1', 'test1@gmail.com', NULL, '$2y$10$o97s0pL4xsV34Z6zalGq5eBWlbT8lI11nCBvbjPjcccY5p7lhQX6i', 'admin', NULL, 'camera.png', NULL, '2019-05-22 05:27:08', '2019-05-22 05:27:08');
 
 --
 -- Индексы сохранённых таблиц
@@ -412,7 +427,7 @@ ALTER TABLE `agencies`
 -- AUTO_INCREMENT для таблицы `artists`
 --
 ALTER TABLE `artists`
-  MODIFY `artist_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `artist_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT для таблицы `main_contents`
@@ -430,19 +445,19 @@ ALTER TABLE `main_title_lefts`
 -- AUTO_INCREMENT для таблицы `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT для таблицы `news_artists`
 --
 ALTER TABLE `news_artists`
-  MODIFY `news_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `news_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT для таблицы `news_contents`
 --
 ALTER TABLE `news_contents`
-  MODIFY `content_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `content_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT для таблицы `oauth_clients`
@@ -460,7 +475,7 @@ ALTER TABLE `oauth_personal_access_clients`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
