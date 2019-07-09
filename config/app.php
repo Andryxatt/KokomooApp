77875f618ee981e7b2c4d2777532739c3afb4df0
Andrii <vlasandrey878@gmail.com>
2019-07-09 13:55:24 +0300

    09.07.2019 (last add changes)

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Chencha\Share\ShareServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -226,7 +226,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Newsletter' => 'Spatie\Newsletter\NewsletterFacade',
-
+        'Share' => Chencha\Share\ShareFacade::class,
     ],
 
 ];
